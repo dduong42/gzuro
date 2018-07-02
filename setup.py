a5d09ea32d7a912bb39d651ba48bbcdf801ac6a7
Daniel Duong <daniel.duong@outlook.com>
2018-07-02 15:17:18 +0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='gzuro',
+    version='0.1',
+    description='A simple Graphical User Interface framework',
+    license='MIT',
+    py_modules=['gzuro'],
+)
